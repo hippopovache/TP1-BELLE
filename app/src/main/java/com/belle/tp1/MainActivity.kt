76be8e4c11f17755e976a725a0733829d22de7ca
maxime belle <45199116+hippopovache@users.noreply.github.com>
2020-12-01 11:34:54 +0100

    third commit

--- app/src/main/java/com/belle/tp1/MainActivity.kt
+++ app/src/main/java/com/belle/tp1/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.belle.tp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     private var button: Button? = null;
+    var editText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Cette activité est celle par défaut à trouver dans AndroidManifest.xml
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button_second)
+        button?.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra(SecondActivity.EXTRA_TEXT, "Mon Texte")
+
+            editText = findViewById(R.id.editText)
+            intent.putExtra(SecondActivity.EXTRA_TEXT1, editText?.text.toString())
+
+            startActivity(intent)
+
+            //destroy de la première activity pour n'avoir plu que la second.
+            finish()
+        }
 
         Log.d("mainActivity", "onCreate")
     }
@@ -42,6 +58,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+
+        //suppression des références (désalocation)
+        button = null
+
         Log.d("mainActivity", "onDestroy")
     }
 }
--- app/src/main/java/com/belle/tp1/SecondActivity.kt
+++ app/src/main/java/com/belle/tp1/SecondActivity.kt
@@ -2,13 +2,38 @@ package com.belle.tp1
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.log
 
 class SecondActivity : AppCompatActivity() {
+
+    var textView: TextView? = null
+    var textView2: TextView? = null
+
+    companion object{
+        //les const vont dans companion object
+
+        const val EXTRA_TEXT = "EXTRA_TEXT"
+        const val EXTRA_TEXT1 = "EXTRA_TEXT1"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_second)
+
+        textView = findViewById(R.id.textView)
+        textView2 = findViewById(R.id.textView2)
+        val extratext = intent.getStringExtra(EXTRA_TEXT)
+        val extratext1 = intent.getStringExtra(EXTRA_TEXT1)
+
+        textView?.text = extratext
+
+        textView2?.text = intent.getStringExtra(EXTRA_TEXT1)
+
+
 
         Log.d("secondActivity", "onCreate")
     }
